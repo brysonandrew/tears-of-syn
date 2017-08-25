6d0ac179cc02fbbbb5bd61c9340b543e841a9348
Andrew <andrewbryson12@gmail.com>
2017-08-25 08:12:37 +0200

    made images full screen

--- src/server.tsx
+++ src/server.tsx
@@ -50,7 +50,7 @@ app.use(favicon(path.join(__dirname, 'public/favicon.ico')));
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-app.use('/images', express.static('./public/images'));
+app.use('/images', express.static('./assets/images'));
 
 app.get('*', (req, res) => {
   const location = req.url;
